a4dd7d2f14bb5d73510cefc3a442f27e23a1e830
Edvaldo Caculo <edvaldocaculo2@gmail.com>
2022-10-14 16:37:29 -0300

    atualizado recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='recipe-home'),
     path('recipes/<int:id>/', views.recipe, name='recipe-details'),
+    path('recipes/category/<int:category_id>/', views.categoria, name='category'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from utils.recipes.factory import make_recipe
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+#from django.http import HttpResponse
+#from utils.recipes.factory import make_recipe
+from .models import Category, Recipes
 
 def home(request):
+    recipes = Recipes.objects.filter(is_published = True,).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes':[make_recipe() for _ in range(10)],
+        'recipes':recipes,
         })
 
-def recipe(request, id):
+def recipe(request, id):  # Essa função buscA O ID da recipes
+    recipe = get_object_or_404(Recipes, 
+        pk= id,
+        is_published = True,
+        )
+        
     return render(request, 'recipes/pages/recipe_views.html', context={
-        'recipe':make_recipe(),
+        'recipe':recipe,
         'is_details_page':True
         })
+
+def categoria(request, category_id):
+    recipes = get_list_or_404(Recipes.objects.filter(
+        category__id= category_id,
+        is_published = True,
+        ).order_by('-id')
+        )
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes':recipes,
+        'title': f'{recipes[0].category.name } - categoria |', # Filtra o campo do titulo da categoria por nome
+        })
+
